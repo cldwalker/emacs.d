cbf65dfa5ca777bc729a4928cc36573b3c55ce01
Gabriel Horner <gabriel.horner@gmail.com>
2013-01-31 00:59:14 -0500

    add nrepl

--- me.el
+++ me.el
@@ -1,3 +1,5 @@
 (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 
+(when (not (package-installed-p 'nrepl))
+    (package-install 'nrepl))
